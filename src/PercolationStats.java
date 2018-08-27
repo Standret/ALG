56c8fc07d53350c83bb251e050e6a6028e1c339d
Standret <pstandret@gmail.com>
2018-08-27 17:27:38 +0300

    completed first task

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,5 +1,8 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
+
+import java.util.Map;
 
 public class PercolationStatistics {
 
@@ -8,31 +11,47 @@ public class PercolationStatistics {
     private int _t;
 
     public static void main(String[] args) {
-        int t = 100;
-        PercolationStatistics stat = new PercolationStatistics(200, t);
+        int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+        PercolationStatistics stat = new PercolationStatistics(n, t);
+
+        System.out.println("start");
+        Stopwatch temp = new Stopwatch();
         stat.doSimulate();
 
         System.out.println("mean                    = " + stat.mean());
         System.out.println("stddev                  = " + stat.stdsev());
         System.out.println("95% confidence interval = [" + (stat.mean() - (1.96 * Math.sqrt(stat.stdsev())) / Math.sqrt(t)) + ", " + (stat.mean() + (1.96 * Math.sqrt(stat.stdsev())) / Math.sqrt(t)) + "]");
+        System.out.println ("elaspes time: " + temp.elapsedTime());
     }
 
     public PercolationStatistics(int n, int t) {
+        if (n < 0 || t < 0)
+             throw new IllegalArgumentException(n + ", " + t);
         _steepValue = new double[t];
-        _n = 200;
-        _t = 100;
+        _n = n;
+        _t = t;
     }
 
     public void doSimulate() {
         for (int i = 0; i < _t; ++i) {
+
             Percolation perc = new Percolation(_n);
-            for (int j = 1; j <= _n; ++j) {
-                perc.open(StdRandom.uniform(1, _n), StdRandom.uniform(1, _n));
-                if (perc.percolates()) {
-                    _steepValue[i] = _n / j;
-                    break;
-                }
+
+            int runs = 0;
+            for (; i < _n * _n && !perc.percolates(); ++runs) {
+                int column;
+                int row;
+
+                do {
+                    column = 1 + StdRandom.uniform(_n);
+                    row = 1 + StdRandom.uniform(_n);
+                } while (perc.isOpen(row, column));
+
+                perc.open(row, column);
             }
+
+            _steepValue[i] = runs / (double)  (_n * _n);
         }
     }
 
